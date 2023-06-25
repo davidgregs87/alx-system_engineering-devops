ad8c497132f440b54dfdb7de868a0768f4c2c722
davidgregs87 <udohdavid27@gmail.com>
2023-06-25 06:06:44 -0700

    Create /tmp/school file with Puppet

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,16 @@
+# Using Puppet, create a file in /tmp
+# Requirements:
+# - File path is /tmp/school
+# - File permission is 0744
+# - File owner is www-data
+# - File group is www-data
+# - File contains "I love Puppet"
+
+file { '/tmp/school':
+  ensure  => present,
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
+
