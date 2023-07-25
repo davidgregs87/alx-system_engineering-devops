c6e3578d7dba0d350abc85d9b31e52eb5e14da44
David Gregs <108700012+davidgregs87@users.noreply.github.com>
2023-07-25 01:56:24 +0100

    Update 0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,29 +1,63 @@
 #!/usr/bin/python3
-"""A python script to gather data from an API"""
+'''
+module: 0-gather_data_from_an_API.py
+This script interacts with a RESTful API to retrieve
+and display TODO list progress for a given employee ID.
+
+Usage:
+    python3 0-gather_data_from_an_API.py <employee_id>
+'''
+
 import requests
 import sys
 
-api = "https://jsonplaceholder.typicode.com"
 
-if __name__ == "__main__":
+def todo_progress(user_id):
+    '''
+    Retrieve and display TODO list progress for the specified employee.
+
+    Args:
+        user_id (int): The ID of the employee.
+    Returns:
+        None
+    '''
+
+    base_url = 'https://jsonplaceholder.typicode.com'
+    user_url = f'{base_url}/users/{user_id}'
+    todos_url = f'{base_url}/todos?userId={user_id}'
+
     try:
-        emp_id = int(sys.argv[1])
-        emp_url = requests.get("{}/users/{}".format(api, emp_id))
-        todo_url = requests.get("{}/todos".format(api)).json()
-        emp_name = emp_url.json().get('name')
-        done_todo = []
-        undone_todo = []
-        for items in todo_url:
-            if items["userId"] == emp_id:
-                if items["completed"]:
-                    done_todo.append(items['title'])
-                else:
-                    undone_todo.append(items['title'])
-        done = len(done_todo)
-        undone = len(undone_todo)
-        print("Employee {} is done with tasks({}/{}):"
-              .format(emp_name, done, done+undone))
-        for line in done_todo:
-            print(f"\t "+line)
-    except Exception:
-        pass
+        user_res = requests.get(user_url)
+        todos_res = requests.get(todos_url)
+
+        if user_res.status_code != 200 or todos_res.status_code != 200:
+            print('Error: Failed to retrieve data from API.')
+            sys.exit(1)
+
+        user_data = user_res.json()
+        todos_data = todos_res.json()
+
+        employee_name = user_data.get('name')
+        total_tasks = len(todos_data)
+        done_tasks = sum(1 for task in todos_data if task.get('completed'))
+
+        print(f'Employee {employee_name} is done '
+              f'with tasks({done_tasks}/{total_tasks}):')
+
+        for task in todos_data:
+            if task.get('completed'):
+                print(f"\t {task.get('title')}")
+
+    except requests.exceptions.RequestException as e:
+        print(f"Error: {e}")
+        sys.exit(1)
+
+
+if __name__ == '__main__':
+    user_id = sys.argv[1]
+
+    if not user_id.isdigit():
+        print("Error: Employee ID must be an integer.")
+        sys.exit(1)
+
+    todo_progress(int(user_id))
