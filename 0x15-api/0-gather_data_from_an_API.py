0dcdc76fb8117c31a2a5c0ceda3fede970da00f7
davidgregs87 <udohdavid27@gmail.com>
2023-07-25 01:44:58 +0100

    fixed task 1

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -6,21 +6,24 @@ import sys
 api = "https://jsonplaceholder.typicode.com"
 
 if __name__ == "__main__":
-    emp_id = int(sys.argv[1])
-    emp_url = requests.get("{}/users/{}".format(api, emp_id))
-    todo_url = requests.get("{}/todos".format(api)).json()
-    emp_name = emp_url.json().get('name')
-    done_todo = []
-    undone_todo = []
-    for items in todo_url:
-        if items["userId"] == emp_id:
-            if items["completed"]:
-                done_todo.append(items['title'])
-            else:
-                undone_todo.append(items['title'])
-    done = len(done_todo)
-    undone = len(undone_todo)
-    print("Employee {} is done with tasks({}/{}):"
-          .format(emp_name, done, done+undone))
-    for line in done_todo:
-        print(f"\t{line}")
+    try:
+        emp_id = int(sys.argv[1])
+        emp_url = requests.get("{}/users/{}".format(api, emp_id))
+        todo_url = requests.get("{}/todos".format(api)).json()
+        emp_name = emp_url.json().get('name')
+        done_todo = []
+        undone_todo = []
+        for items in todo_url:
+            if items["userId"] == emp_id:
+                if items["completed"]:
+                    done_todo.append(items['title'])
+                else:
+                    undone_todo.append(items['title'])
+        done = len(done_todo)
+        undone = len(undone_todo)
+        print("Employee {} is done with tasks({}/{}):"
+              .format(emp_name, done, done+undone))
+        for line in done_todo:
+            print(f"\t "+line)
+    except Exception:
+        pass
