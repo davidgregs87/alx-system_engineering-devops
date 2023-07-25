b8782528752248ceb346c2044293e90ef89506c7
davidgregs87 <udohdavid27@gmail.com>
2023-07-25 03:28:34 +0100

    fixed pycodestyle

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -7,14 +7,13 @@ import sys
 
 url = 'https://jsonplaceholder.typicode.com'
 if __name__ == '__main__':
-        user_id = sys.argv[1]
-        emp_url = requests.get(f"{url}/users/{user_id}").json()
-        todo_url = requests.get(f"{url}/todos", params={"userId": user_id}).json()
-        emp_uname = emp_url.get('username')
-        
-        with open(f"{user_id}.csv", "w", newline='') as csvfile:
-                writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-                for t in todo_url:
-                        writer.writerow([user_id, emp_uname,
-                                         t.get("completed"), t.get("title")])
+    user_id = sys.argv[1]
+    emp_url = requests.get(f"{url}/users/{user_id}").json()
+    todo_url = requests.get(f"{url}/todos", params={"userId": user_id}).json()
+    emp_uname = emp_url.get('username')
 
+    with open(f"{user_id}.csv", "w", newline='') as csvfile:
+        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        for t in todo_url:
+            writer.writerow([user_id, emp_uname,
+                             t.get("completed"), t.get("title")])
