907b5a96e89ce5688190ce20dd4908694c9b5625
davidgregs87 <udohdavid27@gmail.com>
2023-08-09 17:31:33 +0100

    fixed recurse API calls

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,25 +1,46 @@
 #!/usr/bin/python3
-'''
-A scripts that returns a list containing the titles of all hot
-articles for a given subreddit
-'''
+""" This script has  recursive function that queries the
+    Reddit API and returns a list containing the titles of
+    all hot articles for a given subreddit. If no results
+    are found for the given subreddit, the function should
+    return None.
+"""
 import requests
 
+def recurse(subreddit, hot_list=None, after=None):
+    # Set a custom User-Agent to avoid Too Many Requests error
+    headers = {'User-Agent': 'Custom User-Agent'}
 
-def recurse(subreddit, hot_list=[], count=0, after=''):
-    ''' subreddit hot list '''
-    url = 'https://www.reddit.com/r/' + subreddit + '/hot/.json'
-    resp = requests.get(url, headers={'User-Agent': 'dive'},
-                        params={'after': after,
-                                'count': count})
-    if resp.status_code == 404:
-        return None
-    resp = resp.json()
-    res = resp.get('data').get('children')
-    for x in res:
-        hot_list.append(x.get('data').get('title'))
-    after = resp.get('data').get('after')
-    count += resp.get('data').get('dist')
+    if hot_list is None:
+        hot_list = []
+
+    # Construct the URL for the subreddit's hot posts API endpoint
+    url = f'https://www.reddit.com/r/{subreddit}/hot.json?limit=100'
     if after:
-        return recurse(subreddit, hot_list, count, after)
-    return hot_list
+        url += f'&after={after}'
+
+    # Send a GET request to the API endpoint
+    response = requests.get(url, headers=headers)
+
+    # Check if the response is successful (status code 200)
+    if response.status_code == 200:
+        # Parse the JSON response
+        data = response.json()
+
+        # Extract the list of posts
+        posts = data['data']['children']
+
+        # Add titles to the hot_list
+        for post in posts:
+            hot_list.append(post['data']['title'])
+
+        # Check if there's more data to fetch
+        after = data['data']['after']
+        if after:
+            # Recurse with the new 'after' parameter
+            return recurse(subreddit, hot_list, after)
+        else:
+            return hot_list
+    else:
+        # If the response is not successful, return None
+        return None
