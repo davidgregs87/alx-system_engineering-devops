acae6e7b9a9075ca898958417a00c685c293d7a5
davidgregs87 <udohdavid27@gmail.com>
2023-08-17 14:25:06 +0000

    fixed nginx server

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,15 +1,8 @@
-file { '/etc/default/nginx':
-  ensure => file,
-  before => Exec['restart-nginx'],
-  content => template('my_module/nginx_default.erb'), # Use a template with the necessary configuration
-}
+# manifest file: 0-the_sky_is_the_limit_solved.pp
+# increase the open file limit for Nginx using the exec resource:
 
-exec { 'restart-nginx':
-  command => 'service nginx restart',
+exec { 'fix--for-nginx':
+  onlyif  => 'test -e /etc/default/nginx',
+  command => 'sed -i "5s/[0-9]\+/$( ulimit -n )/" /etc/default/nginx; service nginx restart',
   path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-  refreshonly => true,
 }
-
-# You can place the file content in a template located in your module's templates directory
-# my_module/templates/nginx_default.erb
-# ulimit -n=<%= scope.function_ulimit(['nofile']) %>
